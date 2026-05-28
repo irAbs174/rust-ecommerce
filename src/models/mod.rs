b0fd015fe071e92f758d8595a069fa0d6665c2fd
Abbas Damerchilo <damerchiloa@gmail.com>
2026-05-28 19:03:53 -0400

    Add models module

---
+++ src/models/mod.rs
@@ -0,0 +1,15 @@
+pub mod user;
+pub mod product;
+pub mod category;
+pub mod cart;
+pub mod order;
+pub mod review;
+pub mod address;
+
+pub use user::*;
+pub use product::*;
+pub use category::*;
+pub use cart::*;
+pub use order::*;
+pub use review::*;
+pub use address::*;
